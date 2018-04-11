dc4b67a50f046b26ceee898a676ad6e94e7f0f4f
shaiter <shiterartyom@gmail.com>
2018-04-11 13:47:23 +0300

    Initial Commit

---
+++ BSUIR Shedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BSUIR Shedule
+//
+//  Created by Артём Шайтер on 4/11/18.
+//  Copyright © 2018 Артём Шайтер. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
